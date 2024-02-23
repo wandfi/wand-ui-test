6ae972e06859919db1cc3042a42f6a4622a803f8
nonolooklook <nonolooklook@users.noreply.github.com>
2024-02-23 03:02:02 +0000

    deploy: wandfi/wand-ui@c99ef6c24ef52a6b22141435b9e210afae72a23c

--- _next/static/chunks/app/admin/page-4546cecb09d8a473.js
+++
@@ -1,9 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[3],{65967:function(e,r,n){Promise.resolve().then(n.bind(n,5970))},5970:function(e,r,n){"use strict";n.r(r),n.d(r,{default:function(){return AdminPage}});var t=n(57437);function AdminPage(){return(0,t.jsx)("div",{})}},30622:function(e,r,n){"use strict";/**
- * @license React
- * react-jsx-runtime.production.min.js
- *
- * Copyright (c) Meta Platforms, Inc. and affiliates.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */var t=n(2265),o=Symbol.for("react.element"),u=Symbol.for("react.fragment"),f=Object.prototype.hasOwnProperty,i=t.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED.ReactCurrentOwner,s={key:!0,ref:!0,__self:!0,__source:!0};function q(e,r,n){var t,u={},_=null,c=null;for(t in void 0!==n&&(_=""+n),void 0!==r.key&&(_=""+r.key),void 0!==r.ref&&(c=r.ref),r)f.call(r,t)&&!s.hasOwnProperty(t)&&(u[t]=r[t]);if(e&&e.defaultProps)for(t in r=e.defaultProps)void 0===u[t]&&(u[t]=r[t]);return{$$typeof:o,type:e,key:_,ref:c,props:u,_owner:i.current}}r.Fragment=u,r.jsx=q,r.jsxs=q},57437:function(e,r,n){"use strict";e.exports=n(30622)}},function(e){e.O(0,[971,472,744],function(){return e(e.s=65967)}),_N_E=e.O()}]);
