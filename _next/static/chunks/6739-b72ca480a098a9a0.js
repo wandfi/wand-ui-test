3038a15cea37f705f4e66a7d96c47ea6da0291a7
nonolooklook <nonolooklook@users.noreply.github.com>
2024-03-01 04:54:52 +0000

    deploy: wandfi/wand-ui@725d6c8bb35d21a276dd72c4a30d9af84934fa0b

--- _next/static/chunks/6739-b72ca480a098a9a0.js
+++
@@ -1,9 +0,0 @@
-"use strict";(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[6739],{30622:function(r,e,t){/**
- * @license React
- * react-jsx-runtime.production.min.js
- *
- * Copyright (c) Meta Platforms, Inc. and affiliates.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */var n=t(2265),o=Symbol.for("react.element"),a=Symbol.for("react.fragment"),i=Object.prototype.hasOwnProperty,f=n.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED.ReactCurrentOwner,l={key:!0,ref:!0,__self:!0,__source:!0};function q(r,e,t){var n,a={},u=null,s=null;for(n in void 0!==t&&(u=""+t),void 0!==e.key&&(u=""+e.key),void 0!==e.ref&&(s=e.ref),e)i.call(e,n)&&!l.hasOwnProperty(n)&&(a[n]=e[n]);if(r&&r.defaultProps)for(n in e=r.defaultProps)void 0===a[n]&&(a[n]=e[n]);return{$$typeof:o,type:r,key:u,ref:s,props:a,_owner:f.current}}e.Fragment=a,e.jsx=q,e.jsxs=q},57437:function(r,e,t){r.exports=t(30622)},88920:function(r,e,t){t.d(e,{oQC:function(){return FiDollarSign}});var n=t(91172);function FiDollarSign(r){return(0,n.w_)({tag:"svg",attr:{viewBox:"0 0 24 24",fill:"none",stroke:"currentColor",strokeWidth:"2",strokeLinecap:"round",strokeLinejoin:"round"},child:[{tag:"line",attr:{x1:"12",y1:"1",x2:"12",y2:"23"}},{tag:"path",attr:{d:"M17 5H9.5a3.5 3.5 0 0 0 0 7h5a3.5 3.5 0 0 1 0 7H6"}}]})(r)}}}]);
